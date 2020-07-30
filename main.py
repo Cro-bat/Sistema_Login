df9ef850df30fd4af6c0129a30e316ec0078d17f
Tony <tonyram9906@hotmail.com>
2020-07-29 19:47:13 -0500

    "Inicio y creacion de txt"

---
+++ main.py
@@ -0,0 +1,15 @@
+#Abrir archivo txt
+Usuarios = open("Usuarios.txt", "a")
+
+#Preguntar login/register
+    #Si login
+        #revisar usuario
+        #revisar contraseña
+        #entrar/negar acceso
+    #Si register
+        #preguntar usuario
+        #preguntar contraseña
+        #revisar usuario
+        #revisar contraseña
+        #registrar/negar registro
+Usuarios.close()
