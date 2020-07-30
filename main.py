fabcdcc4d63d4cd2b2ac66c32080bd9ba83ef043
Tony <tonyram9906@hotmail.com>
2020-07-30 12:48:37 -0500

    "Terminado"

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 continuar = True
-while continuar == True:
-    #Preguntar login/register
+while continuar:
+    # Preguntar login/register
     entrada = input("Bievenido, ¿Qué acción desea efectuar? Login/Register/Eliminar Registros\n").lower()
     entradas_posibles = ["login", "register", "eliminar registros", "eliminar"]
 
     while entrada not in entradas_posibles:
         entrada = input("Entrada inválida. ¿Qué acción desea efectuar? Login/Register/Eliminar Registros\n").lower()
 
-        #Si register
+        # Si register
     if entrada == "register":
         usuarios = open("Usuarios.txt", "a")
         nuevo_usuario = input("Introduzca nuevo usuario: ")
@@ -17,32 +17,31 @@ while continuar == True:
                 nuevo_usuario = input("Usuario ya existente. Introduzca nuevo usuario: ")
         usuarios.write(nuevo_usuario + ",")
         usuarios.close()
-            #revisar contraseña
+        # revisar contraseña
         with open("Usuarios.txt", "a") as usuarios:
             nueva_contra = ""
             while len(nueva_contra) <= 6:
                 nueva_contra = input("Introduzca contraseña (más de 6 caracteres):\n")
             usuarios.write(nueva_contra + "\n")
-        #aceptar/negar registro
+        # aceptar/negar registro
         print(f"\nFelicidades su usuario ha quedado registrado como:\n"
               f"Usuario: {nuevo_usuario}\n"
               f"Contraseña: {nueva_contra}")
 
-        #Si login
+        # Si login
     elif entrada == "login":
-            #preguntar usuario
+        # preguntar usuario
         with open("Usuarios.txt", "r") as usuarios:
             usuario_actual = input("Introduzca su nombre de usuario: ")
-            #while usuario_actual not in usuarios:
-                #usuario_actual = input("Usuario no encontrado, introduzca un usuario existente: ")
-            #preguntar contraseña
+            # while usuario_actual not in usuarios:
+            #     usuario_actual = input("Usuario no encontrado, introduzca un usuario existente: ")
+            # preguntar contraseña
             contra_actual = input("Introduzca contraseña: ")
-            print(f"{usuario_actual},{contra_actual}")
-            #revisar usuario
-            if (f"{usuario_actual},{contra_actual}") in usuarios:
-                #aceptar ingreso
-                print(f"Acceso concedido, bienvenido {usuario_actual}")
-                #denegar ingreso
+            # revisar usuario
+            if f"{usuario_actual},{contra_actual}\n" in usuarios:
+                # aceptar ingreso
+                print(f"Acceso concedido, bienvenido {usuario_actual}.")
+                # denegar ingreso
             else:
                 print("Acceso denegado, intentelo de nuevo.")
 
@@ -54,7 +53,7 @@ while continuar == True:
             usuarios.write("")
             print("Registros eliminados con éxito.")
 
-    #Preguntar repetir
+    # Preguntar repetir
     pregunta_continuar = input("¿Desea continuar?\n").lower()
     if pregunta_continuar not in ["yes", "si", "y", "s"]:
         continuar = False
--- test.py
+++
@@ -1,6 +0,0 @@
-prueba = "Tony,Mierda123"
-with open("Usuarios.txt", "r") as usuarios:
-    if prueba in usuarios:
-        print("si")
-    else:
-        print("no")
